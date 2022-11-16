122c2e4bb74db367fcbbefe809c6153ff5688d20
Fluturecode <fluturecode@gmail.com>
2022-11-16 17:02:46 -0500

    added logical for isExecutable

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,20 +5,32 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
 import * as web3 from '@solana/web3.js'
+import { getMaxAge } from 'next/dist/server/image-optimizer'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState(false)
 
   const addressSubmittedHandler = (address: string) => {
-    const key = new web3.PublicKey(address)
-    setAddress(key.toBase58())
+    try {
+      setAddress(address)
+      const key = new web3.PublicKey(address)
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
 
-    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+      connection.getBalance(key).then(balance => {
+        setBalance(balance / web3.LAMPORTS_PER_SOL)
+      })
 
-    connection.getBalance(key).then(balance => {
-      setBalance(balance / web3.LAMPORTS_PER_SOL)
-    })
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false)
+      })
+
+    } catch (err) {
+      setAddress('')
+      setBalance(0)
+      alert(err)
+    }
   }
 
   return (
@@ -30,6 +42,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>Is it executable? {isExecutable ? 'Sure Is!' : 'Nope, Sorry'}</p>
       </header>
     </div>
   )
